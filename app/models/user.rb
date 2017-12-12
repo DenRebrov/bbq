97bb4664780189639482810288536b23c57609ec
DenRebrov <den_rebrov@list.ru>
2017-12-12 15:11:49 +0300

    add delete event from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-  has_many :events
+  has_many :events, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 35 }
 
