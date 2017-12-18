505a5215ccbe87502c252b1ded8c2e47d8d1897a
DenRebrov <den_rebrov@list.ru>
2017-12-18 12:53:22 +0300

    second version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  has_many :events, dependent: :destroy
+  has_many :events
+  has_many :comments
+  has_many :subscriptions
 
   validates :name, presence: true, length: { maximum: 35 }
 
